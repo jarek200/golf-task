f38bad2f015a25d48ddd53eecac92626ee185501
Jarek Wyprzal <jwyprzal@gmail.com>
2024-01-21 20:29:51 +0000

    fix socket

--- src/app/player/player-list/player-list.component.ts
+++ src/app/player/player-list/player-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Player } from 'src/app/models/player';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css'],
 })
-@Injectable({
-  providedIn: 'root',
-})
-export class PlayerListComponent implements OnInit {
+export class PlayerListComponent implements OnInit, OnDestroy {
   player: Partial<Player> = {};
+  private dataSubscription: Subscription | undefined;
+
+  constructor(private playerService: PlayerService) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.dataSubscription = this.playerService
+      .getData()
+      .subscribe((data: any) => {
+        console.log('Data received in component:', data); // Add this line
+        this.player = data;
+      });
   }
 
-  listen(eventName: any): Observable<Player> {
-    return (this.player = eventName);
+  ngOnDestroy() {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
   }
 }
--- src/app/player/player.service.ts
+++ src/app/player/player.service.ts
@@ -1,33 +1,33 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class PlayerService {
-
-//   constructor() { }
-// }
-
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { io } from 'socket.io-client';
-import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  private socket;
+  private socket: any;
+  private url = 'https://mst-full-stack-dev-test.herokuapp.com';
 
   constructor() {
-    this.socket = io(environment.apiUrl);
+    this.socket = io(this.url);
   }
 
-  listen(eventName: string): Observable<any> {
-    return new Observable((subscriber) => {
-      this.socket.on(eventName, (data) => {
-        subscriber.next(data);
+  public getData(): Observable<any> {
+    return new Observable((observer) => {
+      // Change this line to listen to 'data-update' instead of 'data'
+      this.socket.on('data-update', (data: any) => {
+        console.log('Data received:', data);
+        observer.next(data);
       });
+
+      this.socket.on('disconnect', (reason: any) => {
+        console.warn('Socket disconnected:', reason);
+      });
+
+      return () => {
+        this.socket.disconnect();
+      };
     });
   }
 }
